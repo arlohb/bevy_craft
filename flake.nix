11b68c1c5286493794fccd48dd97b8fa29babe23
arlohb <arlo@urwick.co.uk>
2023-08-23 15:07:13 +0100

    Got 3d example working

--- flake.nix
+++ flake.nix
@@ -8,14 +8,33 @@
       let
         pkgs = nixpkgs.legacyPackages."${system}";
       in {
-        devShells.default = pkgs.mkShell {
+        devShells.default = pkgs.mkShell rec {
           buildInputs = with pkgs; [
             cargo
             rustc
             rust-analyzer
             clippy
             rustfmt
+            pkg-config
+
+            # Bevy deps
+            alsa-lib
+            udev
+            vulkan-loader
+
+            # Wayland
+            # Can't get working properly
+            # wayland
+            # libxkbcommon
+
+            # X11
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
           ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
         };
       }
     );
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,114 @@
+use std::f32::consts::PI;
+
+use bevy::{
+    prelude::*,
+    render::render_resource::{Extent3d, TextureDimension, TextureFormat},
+};
+
 fn main() {
-    println!("Hello world");
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_systems(Startup, setup)
+        .add_systems(Update, rotate)
+        .run();
+}
+
+#[derive(Component)]
+struct Shape;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut images: ResMut<Assets<Image>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0., 6., 12.).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+        ..default()
+    });
+
+    let debug_material = materials.add(StandardMaterial {
+        base_color_texture: Some(images.add(uv_debug_texture())),
+        ..default()
+    });
+
+    let shapes = [
+        meshes.add(shape::Cube::default().into()),
+        meshes.add(shape::Box::default().into()),
+        meshes.add(shape::Capsule::default().into()),
+        meshes.add(shape::Torus::default().into()),
+        meshes.add(shape::Cylinder::default().into()),
+        meshes.add(shape::Icosphere::default().try_into().unwrap()),
+        meshes.add(shape::UVSphere::default().into()),
+    ];
+    let shapes_len = shapes.len();
+
+    for (i, shape) in shapes.into_iter().enumerate() {
+        commands.spawn((
+            PbrBundle {
+                mesh: shape,
+                material: debug_material.clone(),
+                transform: Transform::from_xyz(
+                    -15. / 2. + i as f32 / (shapes_len - 1) as f32 * 15.,
+                    2.0,
+                    0.0,
+                )
+                .with_rotation(Quat::from_rotation_x(-PI / 4.)),
+                ..default()
+            },
+            Shape,
+        ));
+    }
+
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 9000.,
+            range: 100.,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(8., 16., 8.),
+        ..default()
+    });
+
+    // Ground plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(50.).into()),
+        material: materials.add(Color::SILVER.into()),
+        ..default()
+    });
+}
+
+fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
+    for mut transform in &mut query {
+        transform.rotate_y(time.delta_seconds() / 2.);
+    }
+}
+
+fn uv_debug_texture() -> Image {
+    const SIZE: usize = 64;
+
+    let mut texture_data = [0; SIZE * SIZE * 4];
+
+    for x in 0..SIZE {
+        for y in 0..SIZE {
+            let i = (y * SIZE + x) * 4;
+
+            texture_data[i] = (255. * x as f32 / SIZE as f32) as u8;
+            texture_data[i + 1] = (255. * y as f32 / SIZE as f32) as u8;
+            texture_data[i + 2] = 255;
+            texture_data[i + 3] = 255;
+        }
+    }
+
+    Image::new_fill(
+        Extent3d {
+            width: SIZE as u32,
+            height: SIZE as u32,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        &texture_data,
+        TextureFormat::Rgba8UnormSrgb,
+    )
 }
